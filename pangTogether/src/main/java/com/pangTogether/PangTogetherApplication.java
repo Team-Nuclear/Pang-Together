96e9cf113a5e3420a3ecbea24a59be00b91989c5
TaeJinKim0930 <tjmax0930@gmail.com>
2024-08-24 17:10:57 +0900

    initial project

---
+++ pangTogether/src/main/java/com/pangTogether/PangTogetherApplication.java
@@ -0,0 +1,13 @@
+package com.pangTogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PangTogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PangTogetherApplication.class, args);
+	}
+
+}
